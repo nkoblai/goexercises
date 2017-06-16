7246f36f0f9f61445fc99f67c2e236966e8ba0b9
Nazarii Koblai <nazarkoblay@gmail.com>
2017-06-16 17:03:43 +0300

    fixed

--- listslicestring/palindrome.go
+++ listslicestring/palindrome.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // IsPalindrome tests whether a string is a palindrome.
 func IsPalindrome(s string) bool {
-	for i := 0; i < len(s)/2; i++ {
-		if s[i] == s[len(s)-i-1] {
-			return true
+	res := ""
+	for i := range s {
+		if s[i] >= 97 && s[i] <= 122 || s[i] >= 65 && s[i] <= 90 || s[i] >= 48 && s[i] <= 57 {
+			res += string(s[i])
 		}
 	}
-	return false
+	res = strings.ToLower(res)
+	for i := 0; i < len(res)/2; i++ {
+		if res[i] != res[len(res)-i-1] {
+			return false
+		}
+	}
+	return true
 }
 
 // Write a function that tests whether a string is a palindrome.
 func main() {
-	fmt.Println(IsPalindrome("test false"))
-	fmt.Println(IsPalindrome("lol"))
-	fmt.Println(IsPalindrome("noon"))
+	fmt.Println(IsPalindrome("n1on"))
+	fmt.Println(IsPalindrome("UFO tofu?"))
+	fmt.Println(IsPalindrome("Yo, banana boy!"))
+	fmt.Println(IsPalindrome("Borrow or rob?"))
 }
