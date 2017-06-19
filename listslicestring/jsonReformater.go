6469a329f74be6bed5053066a25bb07b4d8adc0e
Nazarii Koblai <nazarkoblay@gmail.com>
2017-06-19 10:16:29 +0300

    structure agnostic solution

--- listslicestring/jsonReformater.go
+++ listslicestring/jsonReformater.go
@@ -5,32 +5,23 @@ import (
 	"fmt"
 )
 
-// Response represents body of JSON.
-type Response struct {
-	One   []int
-	Three []bool
-	Two   []string
-}
-
-// Reformated represents reformated type.
-type Reformated struct {
-	One   int
-	Three bool
-	Two   string
-}
-
-// JSONReform takes JSON body and returns slice of Reformated type.
-func JSONReform(res Response) []Reformated {
-	ref := make([]Reformated, len(res.One))
-	for i := range res.One {
-		ref[i] = Reformated{One: res.One[i], Three: res.Three[i], Two: res.Two[i]}
-	}
-	return ref
-}
-
 func main() {
 	b := []byte(`{"one":[1,2,3,4,5],"three":[true,false,true,false,true],"two":["1","2","3","4","5"]}`)
-	res := Response{}
-	json.Unmarshal(b, &res)
-	fmt.Println(JSONReform(res))
+	response := make(map[string]interface{})
+	var rez []map[string]interface{}
+	json.Unmarshal(b, &response)
+	flag := false
+	for k, v := range response {
+		vt := v.([]interface{})
+		if !flag {
+			flag = true
+			for range vt {
+				rez = append(rez, make(map[string]interface{}))
+			}
+		}
+		for i := range vt {
+			rez[i][k] = vt[i]
+		}
+	}
+	fmt.Println(rez)
 }
